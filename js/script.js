10a814d70d090ccda6220efa1888711b1056f4ab
Aleksandrs Dubodelovs <54905625+Freeduba@users.noreply.github.com>
2020-04-12 20:29:35 +0300

    Add project

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// Создаем кнопку "закрыть" и добавляем ее для каждого элемента в списке
+var closeButton = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < closeButton.length; i++) {
+  var getSpan = document.createElement("span");
+  var getText = document.createTextNode("\u00D7"); // Unicode character (multiplication sign)
+  getSpan.className = "close";
+  getSpan.appendChild(getText);
+  closeButton[i].appendChild(getSpan);
+}
+
+// Кликаем на кнопку "закрыть" чтобы убрать данный элемент в списке
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Добавляем символ "выполнено" когда кликаем на элемент в списке
+var checkedList = document.querySelector('ul');
+checkedList.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Добавляем новый элемент в список
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
